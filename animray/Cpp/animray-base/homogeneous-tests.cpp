974eefd731c8cda95c3a2c91fc2e5b0c8a46d7bc
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-07 16:44:45 +0000

    Started to put together how the homogenous coordinates are going to be stored internally.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38006 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Cpp/animray-base/homogeneous-tests.cpp
+++ animray/Cpp/animray-base/homogeneous-tests.cpp
@@ -1,9 +1,26 @@
 /*
-    Copyright 1991-2010, Kirit Saelensminde. http://www.kirit.com/AnimRay
+    Copyright 2010, Kirit Saelensminde. http://www.kirit.com/AnimRay
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
 */
 
 
-#include "animray-base.hpp"
+#include <animray/homogeneous>
+#include <fost/test>
+
+
+FSL_TEST_SUITE( homogeneous );
+
+
+FSL_TEST_FUNCTION( constructors ) {
+    animray::homogeneous< int > h;
+    FSL_CHECK_EQ( h.coords()[0], 0.);
+    FSL_CHECK_EQ( h.coords()[1], 0.);
+    FSL_CHECK_EQ( h.coords()[2], 0.);
+    FSL_CHECK_EQ( h.coords()[3], 1.);
+    FSL_CHECK_EQ( h.x(), 0.);
+    FSL_CHECK_EQ( h.y(), 0.);
+    FSL_CHECK_EQ( h.z(), 0.);
+}
+
