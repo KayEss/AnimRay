c30611e49082ab7be5e2ce979873faac0e6a0de8
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-07 16:56:14 +0000

    Added some tests for equality and inequality.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38008 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Cpp/animray-base/homogeneous-tests.cpp
+++ animray/Cpp/animray-base/homogeneous-tests.cpp
@@ -13,14 +13,28 @@
 FSL_TEST_SUITE( homogeneous );
 
 
+FSL_TEST_FUNCTION( constructor_default_tests ) {
+    fostlib::test::default_copy_constructable< animray::homogeneous< int > >();
+    fostlib::test::default_copy_constructable< animray::homogeneous< int64_t > >();
+    fostlib::test::default_copy_constructable< animray::homogeneous< float > >();
+    fostlib::test::default_copy_constructable< animray::homogeneous< double > >();
+    fostlib::test::default_copy_constructable< animray::homogeneous< long double > >();
+}
+
+
 FSL_TEST_FUNCTION( constructors ) {
-    animray::homogeneous< int > h;
-    FSL_CHECK_EQ( h.coords()[0], 0.);
-    FSL_CHECK_EQ( h.coords()[1], 0.);
-    FSL_CHECK_EQ( h.coords()[2], 0.);
-    FSL_CHECK_EQ( h.coords()[3], 1.);
-    FSL_CHECK_EQ( h.x(), 0.);
-    FSL_CHECK_EQ( h.y(), 0.);
-    FSL_CHECK_EQ( h.z(), 0.);
+    animray::homogeneous< int > h1;
+    FSL_CHECK_EQ( h1.coords()[0], 0.);
+    FSL_CHECK_EQ( h1.coords()[1], 0.);
+    FSL_CHECK_EQ( h1.coords()[2], 0.);
+    FSL_CHECK_EQ( h1.coords()[3], 1.);
+    FSL_CHECK_EQ( h1.x(), 0.);
+    FSL_CHECK_EQ( h1.y(), 0.);
+    FSL_CHECK_EQ( h1.z(), 0.);
+
+    animray::homogeneous< float > h2( 1, 2, 3 );
+    FSL_CHECK_EQ( h2.x(), 1 );
+    FSL_CHECK_EQ( h2.y(), 2 );
+    FSL_CHECK_EQ( h2.z(), 3 );
 }
 
