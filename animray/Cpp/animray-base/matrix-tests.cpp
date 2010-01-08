9bb035547fcfa482063c282d6345e22e6589eb79
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-08 14:27:23 +0000

    This works, but the friendship is probably too broad. It might be better to insist on a named accessor that the JSON casting can use.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38022 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Cpp/animray-base/matrix-tests.cpp
+++ animray/Cpp/animray-base/matrix-tests.cpp
@@ -20,3 +20,12 @@ FSL_TEST_FUNCTION( constructor_default_tests ) {
     fostlib::test::default_copy_constructable< animray::matrix< double > >();
     fostlib::test::default_copy_constructable< animray::matrix< long double > >();
 }
+
+
+FSL_TEST_FUNCTION( json ) {
+    FSL_CHECK_EQ(
+        fostlib::coerce< fostlib::json >( animray::matrix< int64_t >() ),
+        fostlib::json::parse(L"[1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]")
+    );
+}
+
