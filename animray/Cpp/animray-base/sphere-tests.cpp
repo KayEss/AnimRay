aad00c0c64909b5e1647f8168048d53c3b41127c
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-08 15:47:29 +0000

    Added a skeleton for a sphere template to do intersection tests with.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38030 8dc44086-f19e-e14e-beb8-9da1b3ac110d

---
+++ animray/Cpp/animray-base/sphere-tests.cpp
@@ -0,0 +1,22 @@
+/*
+    Copyright 2010, Kirit Saelensminde. http://www.kirit.com/AnimRay
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <animray/sphere>
+#include <fost/test>
+
+
+FSL_TEST_SUITE( sphere );
+
+
+FSL_TEST_FUNCTION( constructor_default_tests ) {
+    fostlib::test::default_copy_constructable< animray::sphere< int > >();
+    fostlib::test::default_copy_constructable< animray::sphere< int64_t > >();
+    fostlib::test::default_copy_constructable< animray::sphere< float > >();
+    fostlib::test::default_copy_constructable< animray::sphere< double > >();
+    fostlib::test::default_copy_constructable< animray::sphere< long double > >();
+}
