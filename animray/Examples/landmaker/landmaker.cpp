bd60b8c22600f28a1ef6b3f55087247645130d14
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-09 06:24:25 +0000

    This fails to compile under a large number of Boost versions on Karmic.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38041 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Examples/landmaker/landmaker.cpp
+++ animray/Examples/landmaker/landmaker.cpp
@@ -22,18 +22,27 @@
 #include <fost/unicode>
 #include <fost/main>
 
+#include <boost/gil/image.hpp>
+#include <boost/gil/typedefs.hpp>
+#include <boost/gil/extension/io/png_dynamic_io.hpp>
+
 
 FSL_MAIN(
     L"landmaker",
     L"LandMaker, Copyright 2010 Kirit Saelensminde"
 )( fostlib::ostream &out, fostlib::arguments &args ) {
-    boost::filesystem::wpath output = fostlib::coerce< boost::filesystem::wpath >(
-        args[1].value("out.jpeg")
-    );
-    int64_t width = fostlib::coerce< int64_t >( args[2].value("100") );
-    int64_t height = fostlib::coerce< int64_t >( args[3].value("100") );
-    out << "Creating image " << output
+    boost::filesystem::wpath output_filename =
+        fostlib::coerce< boost::filesystem::wpath >(args[1].value("out.jpeg"))
+    ;
+    int width = fostlib::coerce< int >( args[2].value("100") );
+    int height = fostlib::coerce< int >( args[3].value("100") );
+    out << "Creating image " << output_filename
         <<", size " << width << " x " << height << std::endl
     ;
+
+    using namespace boost::gil;
+    rgb8_image_t output(100, 100);
+    png_write_view("out.png", view(output));
+
     return 0;
 }
