c4059d2e27aca640327b58b5866846dee4e8abcd
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-09 05:30:52 +0000

    Started to sketch out the landmaker example.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38040 8dc44086-f19e-e14e-beb8-9da1b3ac110d

---
+++ animray/Examples/landmaker/landmaker.cpp
@@ -0,0 +1,39 @@
+/*
+    Copyright 2010, Kirit Saelensminde.
+    http://www.kirit.com/AnimRay
+
+    This file is part of AnimRay.
+
+    AnimRay is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AnimRay is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AnimRay.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <fost/unicode>
+#include <fost/main>
+
+
+FSL_MAIN(
+    L"landmaker",
+    L"LandMaker, Copyright 2010 Kirit Saelensminde"
+)( fostlib::ostream &out, fostlib::arguments &args ) {
+    boost::filesystem::wpath output = fostlib::coerce< boost::filesystem::wpath >(
+        args[1].value("out.jpeg")
+    );
+    int64_t width = fostlib::coerce< int64_t >( args[2].value("100") );
+    int64_t height = fostlib::coerce< int64_t >( args[3].value("100") );
+    out << "Creating image " << output
+        <<", size " << width << " x " << height << std::endl
+    ;
+    return 0;
+}
