5bde82022a9463e306576fd6aa838966fe4f3fb9
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-25 12:33:26 +0000

    Added a bit of history to the documentation front page and a brief description of the animray namespace.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38231 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Docs/index.hpp
+++ animray/Docs/index.hpp
@@ -1,4 +1,14 @@
 /** \mainpage
 
     <h1>AnimRay</h1>
+
+    AnimRay was originally started around 1992 as a SmallTalk program. This
+    version was flexible, but very slow. The next version was written in one of
+    the early C++ compilers of the day, Microsoft's first Visual C++ compiler which
+    was for 16 bit Windows.
+*/
+
+/** \namespace animray
+
+    \brief This namespace has all of the ray tracing building blocks.
 */
