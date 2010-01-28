2261db9a11714af66db31a94bc79d6aaa0a8f390
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-01-28 04:52:27 +0000

    Started to sketch out a mandelbrot program.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38244 8dc44086-f19e-e14e-beb8-9da1b3ac110d

---
+++ animray/Examples/mandelbrot/mandelbrot.cpp
@@ -0,0 +1,53 @@
+/*
+    Copyright 2010, Kirit Saelensminde.
+    http://www.kirit.com/AnimRay
+
+    This file is part of AnimRay.
+
+    AnimRay is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AnimRay is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AnimRay.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <fost/unicode>
+#include <fost/main>
+#include <fost/coerce/ints.hpp>
+#include <animray/targa.hpp>
+
+
+namespace {
+}
+
+
+FSL_MAIN(
+    L"landmaker",
+    L"LandMaker, Copyright 2010 Kirit Saelensminde"
+)( fostlib::ostream &out, fostlib::arguments &args ) {
+    boost::filesystem::wpath output_filename =
+        fostlib::coerce< boost::filesystem::wpath >(args[1].value("out.tga"))
+    ;
+    int width = fostlib::coerce< int >( args[2].value("100") );
+    int height = fostlib::coerce< int >( args[3].value("100") );
+    out << "Creating image " << output_filename
+        <<", size " << width << " x " << height << std::endl
+    ;
+
+    typedef animray::film< uint8_t > film_type;
+    film_type output(width, height);
+
+    // Calculation to go here
+
+    animray::targa(output_filename, output);
+
+    return 0;
+}
