1b02eb3da94dd549d84afe4c61f6382f781b25c7
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-02-06 09:12:57 +0000

    Only the Targe image data now depends on the colour format.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38397 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Cpp/include/animray/targa.hpp
+++ animray/Cpp/include/animray/targa.hpp
@@ -44,9 +44,31 @@ namespace animray {
     template< typename C, typename E >
     void targa(
         const boost::filesystem::wpath &filename,
-        const film< C, E > &f
+        const film< C, E > &image
     ) {
-        detail::targa_saver< C, E >()( filename, f );
+        detail::targa_saver< C, E > saver;
+        boost::filesystem::ofstream file( filename, std::ios::binary );
+        // Header
+        file.put(0); // 0 identsize
+        file.put(0); // Has no colour map
+        file.put(3); // Uncompressed grayscale image (as this is uint8_t)
+        file.put(0); file.put(0); // Colour map offset
+        file.put(0); file.put(0); // Colour map indexes
+        file.put(0); // Colour map bits per pixel
+        file.put(0); file.put(0); // X origin
+        file.put(0); file.put(0); // Y origin
+        uint16_t w( fostlib::coerce< uint16_t >(image.width()) );
+        uint16_t h( fostlib::coerce< uint16_t >(image.height()) );
+        file.write(reinterpret_cast< const char * >(&w), 2);
+        file.write(reinterpret_cast< const char * >(&h), 2);
+        file.put(saver.bits); // n bit pixels
+        file.put(0x0); // Image data starts bottom left with zero alpha channel
+        // Image data
+        saver( file, image );
+        // Footer (for Targa 2)
+        file.put(0); file.put(0); // Targa 2 extension data size
+        file << "TRUEVISION-XFILE.";
+        file.put(0);
     }
 
 
@@ -54,35 +76,15 @@ namespace animray {
         /// Save 8 bit films as Targa file
         template< typename E >
         struct targa_saver< uint8_t, E > {
+            const static uint8_t bits = 8;
             void operator () (
-                const boost::filesystem::wpath &filename,
+                std::ostream &file,
                 const film< uint8_t, E > &image
             ) const {
-                boost::filesystem::ofstream file( filename, std::ios::binary );
-                // Header
-                file.put(0); // 0 identsize
-                file.put(0); // Has no colour map
-                file.put(3); // Uncompressed grayscale image (as this is uint8_t)
-                file.put(0); file.put(0); // Colour map offset
-                file.put(0); file.put(0); // Colour map indexes
-                file.put(0); // Colour map bits per pixel
-                file.put(0); file.put(0); // X origin
-                file.put(0); file.put(0); // Y origin
-                uint16_t w( fostlib::coerce< uint16_t >(image.width()) );
-                uint16_t h( fostlib::coerce< uint16_t >(image.height()) );
-                file.write(reinterpret_cast< const char * >(&w), 2);
-                file.write(reinterpret_cast< const char * >(&h), 2);
-                file.put(8); // 8 bit pixels
-                file.put(0x0); // Image data starts bottom left with zero alpha channel
-                // Image data
                 typedef typename film< uint8_t, E >::size_type size_type;
                 for ( size_type r = 0; r < image.height(); ++r )
                     for ( size_type c = 0; c < image.width(); ++c )
                         file.put(image[c][r]);
-                // Footer (for Targa 2)
-                file.put(0); file.put(0); // Targa 2 extension data size
-                file << "TRUEVISION-XFILE.";
-                file.put(0);
             }
         };
     }
