6d5fa4e050bc5764c96dcddf808c5d6d1470cd20
Kirit Sælensminde <Kirit Slensminde@8dc44086-f19e-e14e-beb8-9da1b3ac110d>
2010-02-08 14:16:37 +0000

    Added a little bit more documentation.
    git-svn-id: https://svn.felspar.com/public/AnimRay/trunk@38438 8dc44086-f19e-e14e-beb8-9da1b3ac110d

--- animray/Docs/index.hpp
+++ animray/Docs/index.hpp
@@ -2,13 +2,17 @@
 
     <h1>AnimRay</h1>
 
-    AnimRay was originally started around 1992 as a SmallTalk program. This
-    version was flexible, but very slow. The next version was written in one of
-    the early C++ compilers of the day, Microsoft's first Visual C++ compiler which
-    was for 16 bit Windows.
+    AnimRay was originally started around 1992 as a SmallTalk program.
+    This version was flexible, but very slow. The next version was written
+    in one of the early C++ compilers of the day, Microsoft's first Visual
+    C++ compiler which was for 16 bit Windows.
 */
 
 /** \namespace animray
 
     \brief This namespace has all of the ray tracing building blocks.
+*/
+/** \namespace animray::mandelbrot
+
+    \brief Holds the various parts of the Mandelbrot textures.
 */
