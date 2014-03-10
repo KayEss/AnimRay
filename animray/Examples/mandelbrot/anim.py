ec59bf2cef1f35885d6f64071743aaa6a06c2bc7
Kirit Sælensminde <k@kirit.com>
2014-03-10 09:16:16 +0700

    Fixed the hue tweening.

--- animray/Examples/mandelbrot/anim.py
+++ animray/Examples/mandelbrot/anim.py
@@ -5,7 +5,7 @@ STEPS = 24 * 60
 X = (-0.763, -0.763043)
 Y = (0.096, 0.096087)
 D = (2.0, 0.0003)
-H = (0, 1000)
+H = (0.0, 360.0 * 3)
 
 def tween(step, (s, e)):
     return (e - s) / (STEPS-1) * step + s
@@ -15,6 +15,8 @@ def diam(step, (s, e)):
         ( 1 - pow(abs(e - s), 1.0 / STEPS) ** (STEPS - step) ))
 
 for s in range(0, STEPS):
-    subprocess.call(['dist/bin/mandelbrot', 'out%04d.tga' % (s+1), '1920', '1080',
+    prog = ['dist/bin/mandelbrot', 'out%04d.tga' % (s+1), '1920', '1080',
         '-cx', str(tween(s, X)), '-cy', str(tween(s, Y)), '-d', str(diam(s, D)),
-        '-h', str(tween(s, H))])
+        '-h', str(tween(s, H))]
+    print prog
+    subprocess.call(prog)
