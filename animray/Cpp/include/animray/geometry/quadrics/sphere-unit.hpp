3004bac3fd2eb4a34ae5a4556e827f92e53d30ec
Kirit Sælensminde <k@kirit.com>
2014-10-12 01:32:09 +0700

    Fill out more of what a positionable sphere should look like.

--- animray/Cpp/include/animray/geometry/quadrics/sphere-unit.hpp
+++ animray/Cpp/include/animray/geometry/quadrics/sphere-unit.hpp
@@ -19,8 +19,8 @@
 */
 
 
-#ifndef ANIMRAY_GEOMETRY_QUADRICS_SPHERE_ORIGIN_HPP
-#define ANIMRAY_GEOMETRY_QUADRICS_SPHERE_ORIGIN_HPP
+#ifndef ANIMRAY_GEOMETRY_QUADRICS_SPHERE_UNIT_HPP
+#define ANIMRAY_GEOMETRY_QUADRICS_SPHERE_UNIT_HPP
 #pragma once
 
 
@@ -30,7 +30,35 @@
 namespace animray {
 
 
+    template<typename I, typename D = typename I::local_coord_type>
+    class unit_sphere {
+        /// The sphere at the origin we want to map position to
+        unit_sphere_at_origin<I, D> origin;
+    public:
+        /// The type of the local coordinates used
+        typedef D local_coord_type;
+        /// Type of intersection to return when the sphere is struck
+        typedef I intersection_type;
+
+        /// Set the position of the sphere
+        fostlib::accessors<point3d<D>> position;
+
+        /// Returns a ray giving the intersection point and surface normal or
+        /// null if no intersection occurs
+        template<typename R, typename E>
+        fostlib::nullable< intersection_type > intersects(
+            const R &by, const E epsilon
+        ) const {
+        }
+
+        /// Returns true if the ray hits the sphere
+        template<typename R, typename E>
+        bool occludes(const R &by, const E epsilon) const {
+        }
+    };
+
+
 }
 
 
-#endif // ANIMRAY_GEOMETRY_QUADRICS_SPHERE_ORIGIN_HPP
+#endif // ANIMRAY_GEOMETRY_QUADRICS_SPHERE_UNIT_HPP
