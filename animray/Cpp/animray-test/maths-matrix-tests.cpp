c9eba9a936941182f198ef363dfded2a86cddee8
Kirit Sælensminde <k@kirit.com>
2014-12-06 11:06:42 +0700

    Add tests to make sure that matrix transforms of rays are correct.

--- animray/Cpp/animray-test/maths-matrix-tests.cpp
+++ animray/Cpp/animray-test/maths-matrix-tests.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2010, Kirit Saelensminde.
+    Copyright 2010-2014, Kirit Saelensminde.
     http://www.kirit.com/AnimRay
 
     This file is part of AnimRay.
@@ -20,13 +20,14 @@
 
 
 #include <animray/affine.hpp>
+#include <animray/ray.hpp>
 #include "test-json.hpp"
 
 
-FSL_TEST_SUITE( matrix );
+FSL_TEST_SUITE(matrix);
 
 
-FSL_TEST_FUNCTION( constructor_default_tests ) {
+FSL_TEST_FUNCTION(constructor_default_tests) {
     fostlib::test::default_copy_constructable< animray::matrix< int > >();
     fostlib::test::default_copy_constructable< animray::matrix< int64_t > >();
     fostlib::test::default_copy_constructable< animray::matrix< float > >();
@@ -35,7 +36,7 @@ FSL_TEST_FUNCTION( constructor_default_tests ) {
 }
 
 
-FSL_TEST_FUNCTION( matrix_multiply ) {
+FSL_TEST_FUNCTION(matrix_multiply) {
     animray::matrix< int > id;
     auto aff(animray::translate<int>(10, 23, 54));
     FSL_CHECK_EQ(aff.forward() * aff.backward(), id);
@@ -52,17 +53,63 @@ FSL_TEST_FUNCTION( matrix_multiply ) {
 }
 
 
-FSL_TEST_FUNCTION( json ) {
+FSL_TEST_FUNCTION(json) {
     json_roundtrip(
         animray::matrix< int64_t >(),
-        L"[[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]"
-    );
+        L"[[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]");
     animray::matrix< int64_t > m( fostlib::json::parse(
-        L"[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]"
-    ) );
+        L"[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]") );
     FSL_CHECK_EQ( m[0][0], 1 );
     FSL_CHECK_EQ( m[0][2], 3 );
     FSL_CHECK_EQ( m[3][3], 16 );
     FSL_CHECK_EXCEPTION( m[4][0], fostlib::exceptions::out_of_range<std::size_t>& );
+}
+
+
+FSL_TEST_FUNCTION(ray_rotate_forward) {
+    animray::ray<double> z(
+        animray::point3d<double>(0, 0, 0),
+        animray::unit_vector<double>(0, 0, 1));
+    FSL_CHECK_EQ(z.direction(), (z.ends() - z.from()).unit());
+
+    animray::matrix<double> rx45(animray::rotate_x<double>(-45_deg).first);
+    animray::ray<double> r45(z * rx45);
+    FSL_CHECK_EQ(r45.direction(), (r45.ends() - r45.from()).unit());
+
+    animray::ray<double> r90(r45 * rx45);
+    FSL_CHECK_EQ(r90.direction(), (r90.ends() - r90.from()).unit());
+    FSL_CHECK_ERROR(r90.direction().x(), 0, 1e-10);
+    FSL_CHECK_ERROR(r90.direction().y(), 1, 1e-10);
+    FSL_CHECK(r90.direction().z() < 1e-10);
+}
+
+
+FSL_TEST_FUNCTION(ray_rotate_backward) {
+    animray::ray<double> z(
+        animray::point3d<double>(0, 0, 0),
+        animray::unit_vector<double>(0, 0, 1));
+    FSL_CHECK_EQ(z.direction(), (z.ends() - z.from()).unit());
+
+    animray::matrix<double> rx45(animray::rotate_x<double>(45_deg).second);
+    animray::ray<double> r45(z * rx45);
+    FSL_CHECK_EQ(r45.direction(), (r45.ends() - r45.from()).unit());
+
+    animray::ray<double> r90(r45 * rx45);
+    FSL_CHECK_EQ(r90.direction(), (r90.ends() - r90.from()).unit());
+    FSL_CHECK_ERROR(r90.direction().x(), 0, 1e-10);
+    FSL_CHECK_ERROR(r90.direction().y(), 1, 1e-10);
+    FSL_CHECK(r90.direction().z() < 1e-10);
+}
+
+
+FSL_TEST_FUNCTION(ray_scale) {
+    animray::ray<double> all(
+        animray::point3d<double>(0, 0, 0),
+        animray::point3d<double>(1, 1, 1));
+    FSL_CHECK_EQ(all.direction(), (all.ends() - all.from()).unit());
+
+    animray::matrix<double> halve(animray::scale<double>(0.5, 0.5, 0.5).first);
+    animray::ray<double> havled(all * halve);
+    FSL_CHECK_EQ(all, havled);
 }
 
