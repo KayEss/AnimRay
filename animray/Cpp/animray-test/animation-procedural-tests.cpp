c8929389bfc5febac070ca9dd28b6852335c6ce1
Kirit Sælensminde <k@kirit.com>
2014-12-27 12:52:22 +0700

    Fix the range we're interpolating over.

--- animray/Cpp/animray-test/animation-procedural-tests.cpp
+++ animray/Cpp/animray-test/animation-procedural-tests.cpp
@@ -44,19 +44,19 @@ FSL_TEST_FUNCTION(constant) {
 
 FSL_TEST_FUNCTION(linear_frames_boost_function) {
     boost::function<int(const ray_type &)> f{[](const ray_type &r) {
-        return animray::interpolation::linear(5.0, 15.0, r.frame(), std::size_t(11));
+        return animray::interpolation::linear(5.0, 15.0, r.frame(), std::size_t(10));
     }};
     ray_type ray;
     FSL_CHECK_EQ(animray::value(f, ray), 5);
-    ray.frame(11);
-    FSL_CHECK_EQ(ray.frame(), 11);
+    ray.frame(10);
+    FSL_CHECK_EQ(ray.frame(), 10);
     FSL_CHECK_EQ(animray::value(f, ray), 15);
 }
 
 
 FSL_TEST_FUNCTION(linear_frames_std_function) {
     std::function<int(const ray_type &)> f{[](const ray_type &r) {
-        return animray::interpolation::linear(-5.0, 5.0, r.frame(), std::size_t(11));
+        return animray::interpolation::linear(-5.0, 5.0, r.frame(), std::size_t(10));
     }};
     ray_type ray;
     FSL_CHECK_EQ(animray::value(f, ray), -5);
@@ -67,11 +67,11 @@ FSL_TEST_FUNCTION(linear_frames_std_function) {
 
 FSL_TEST_FUNCTION(linear_frames_auto) {
     auto f = [](const ray_type &r) {
-        return animray::interpolation::linear(-5.0, 5.0, r.frame(), std::size_t(11));
+        return animray::interpolation::linear(-5.0, 5.0, r.frame(), std::size_t(10));
     };
     ray_type ray;
     FSL_CHECK_EQ(animray::value(f, ray), -5);
-    ray.frame(6);
+    ray.frame(5);
     FSL_CHECK_EQ(animray::value(f, ray), 0);
 }
 
