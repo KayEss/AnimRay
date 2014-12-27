b5897219b681b949a96c8307b7c2b0224cffdab0
Kirit Sælensminde <k@kirit.com>
2014-12-27 10:02:05 +0700

    Start to add in procedural animation.

---
+++ animray/Cpp/animray-test/animation-procedural-tests.cpp
@@ -0,0 +1,36 @@
+/*
+    Copyright 2014, Kirit Saelensminde.
+    http://www.kirit.com/AnimRay
+
+    This file is part of AnimRay.
+
+    AnimRay is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AnimRay is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AnimRay.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include <animray/ray.hpp>
+#include <animray/mixins/frame.hpp>
+#include <fost/test>
+
+
+FSL_TEST_SUITE(animation_procedural);
+
+
+FSL_TEST_FUNCTION(linear_frames) {
+    typedef animray::ray<int> base_ray;
+    typedef animray::with_frame<base_ray>::type ray_type;
+    ray_type ray;
+    FSL_CHECK_EQ(ray.frame(), 0);
+}
+
---
+++ animray/Cpp/include/animray/mixins/frame.hpp
@@ -0,0 +1,59 @@
+/*
+    Copyright 2014, Kirit Saelensminde.
+    http://www.kirit.com/AnimRay
+
+    This file is part of AnimRay.
+
+    AnimRay is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    AnimRay is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with AnimRay.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef ANIMRAY_MIXINS_FRAME_HPP
+#define ANIMRAY_MIXINS_FRAME_HPP
+#pragma once
+
+
+#include <animray/mixins/mixin.hpp>
+
+
+namespace animray {
+
+
+    namespace detail {
+        class at_frame {
+        public:
+            at_frame()
+            : frame(0) {
+            }
+
+            fostlib::accessors<std::size_t> frame;
+        };
+    }
+
+
+    /// Mixin for recording a frame number
+    template<typename T>
+    struct with_frame {
+        typedef typename std::conditional<
+                std::is_base_of<detail::at_frame, T>::value,
+                T,
+                mixin<T, detail::at_frame>
+            >::type type;
+    };
+
+
+}
+
+
+#endif // ANIMRAY_MIXINS_FRAME_HPP
