cf0993ce797d96c192e214ba4660e1920f499255
Kirit Sælensminde <k@kirit.com>
2014-12-27 14:07:23 +0700

    Remove a level of indrection it turns out we don't need.

--- animray/Cpp/include/animray/value.hpp
+++ animray/Cpp/include/animray/value.hpp
@@ -65,25 +65,19 @@ namespace animray {
     {};
 
 
-    namespace detail {
-        /// Allow parameters to be applied such as to calculate some value
-        template<typename V, typename... P>
-        V value(const V &v, typename std::enable_if<not is_callable<V>::value>::type*, const P &...) {
-            return v;
-        }
-
-
-        /// Apply the parameters to the function
-        template<typename F, typename... P>
-        decltype(auto) value(const F &f, typename std::enable_if<is_callable<F>::value>::type*, const P &...ps) {
-            return f(ps...);
-        }
+    /// Allow parameters to be applied such as to calculate some value
+    template<typename V, typename... P,
+        typename = typename std::enable_if<not is_callable<V>::value>::type>
+    V value(const V &v, const P &...) {
+        return v;
     }
 
 
-    template<typename C, typename... P>
-    decltype(auto) value(const C &c, const P &...ps) {
-        return detail::value(c, nullptr, ps...);
+    /// Apply the parameters to the function
+    template<typename F, typename... P,\
+        typename = typename std::enable_if<is_callable<F>::value>::type>
+    decltype(auto) value(const F &f, const P &...ps) {
+        return f(ps...);
     }
 
 
