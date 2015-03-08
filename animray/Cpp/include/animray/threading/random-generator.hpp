bfb219c1fbc545a397951f4e62c5a266d26472ef
Kirit Sælensminde <k@kirit.com>
2015-03-08 13:46:01 +0700

    Simplify the way we're using this.

--- animray/Cpp/include/animray/threading/random-generator.hpp
+++ animray/Cpp/include/animray/threading/random-generator.hpp
@@ -28,7 +28,7 @@
 namespace animray {
 
 
-    inline namespace random {
+    namespace random {
 
 
         /// Wrapper for a std:: random engine so it can be used as a template
@@ -46,66 +46,17 @@ namespace animray {
         };
 
 
+        /// Define this outside of the template because the rules say so
         template< typename E, typename D >
         thread_local E engine<E, D>::e{D{}()};
 
 
-        /// Wrapper for a distribution so it can be used as a template
-        /// argument and produce numbers in a thread safe manner
-        template< typename D, const typename D::param_type *P,
-            typename E = engine<> >
-        struct distribution {
-            static typename D::result_type value() {
-                thread_local D d(*P);
-                return d(E::e);
-            }
+        /// Tie together a distribution with the default engine
+        template< typename D, typename E = engine<> >
+        struct with_engine {
+            typedef D distribution;
+            typedef E engine;
         };
-
-
-        /// Template wrapper to store distribution arguments so that they
-        /// can be used as arguments for a template parameter.
-        inline namespace parameters {
-
-
-            /// One unit
-            template< typename D >
-            struct zero_to_one {
-                static const typename D::param_type parameter;
-            };
-            template<>
-            const std::uniform_real_distribution<float>::param_type
-                zero_to_one<std::uniform_real_distribution<float>>::
-                    parameter{0.f, 1.f};
-            template<>
-            const std::uniform_real_distribution<double>::param_type
-                zero_to_one<std::uniform_real_distribution<double>>::
-                    parameter{-0., 1.};
-            template<>
-            const std::uniform_real_distribution<long double>::param_type
-                zero_to_one<std::uniform_real_distribution<long double>>::
-                    parameter{-0.l, 1.l};
-
-
-            /// One unit distributed about the zero point
-            template< typename D >
-            struct plus_minus_half {
-                static const typename D::param_type parameter;
-            };
-            template<>
-            const std::uniform_real_distribution<float>::param_type
-                plus_minus_half<std::uniform_real_distribution<float>>::
-                    parameter{-0.5f, 0.5f};
-            template<>
-            const std::uniform_real_distribution<double>::param_type
-                plus_minus_half<std::uniform_real_distribution<double>>::
-                    parameter{-0.5, 0.5};
-            template<>
-            const std::uniform_real_distribution<long double>::param_type
-                plus_minus_half<std::uniform_real_distribution<long double>>::
-                    parameter{-0.5l, 0.5l};
-
-
-        }
 
 
     }
