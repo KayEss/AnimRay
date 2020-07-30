946008ebf69d6a79469e3a2adbb2ad5dcb11913d
Kirit Sælensminde <kirit@kirit.com>
2020-07-30 17:33:51 +0700

    Output both linear and non-linear versions of the colour blend

--- scenes/colour/mix.cpp
+++ scenes/colour/mix.cpp
@@ -28,6 +28,7 @@ namespace {
     using colour = animray::rgb<float>;
 
     constexpr std::size_t const width = 1920, height = 1080;
+
     constexpr auto const top_left = colour{255, 0, 0};
     constexpr auto const top_right = colour{255, 0, 0};
     constexpr auto const bottom_left = colour{0, 255, 0};
@@ -42,23 +43,44 @@ namespace {
                 animray::interpolation::linear(
                         start.blue(), end.blue(), pos, limit));
     }
-    auto quantise(float channel) {
-        return uint8_t(std::clamp(channel, 0.0f, 255.0f));
+    auto pixel_colour(std::size_t x, std::size_t y) {
+        auto const left = lerp(top_left, bottom_left, y, height);
+        auto const right = lerp(top_right, bottom_right, y, height);
+        return lerp(left, right, x, width);
+    }
+    uint8_t linear_clamp(float channel) {
+        return std::clamp(channel, 0.0f, 255.0f);
+    }
+    uint8_t non_linear_clamp(float channel) {
+        auto const normalised = std::clamp(channel, 0.0f, 255.0f) / 255.0f;
+        if (normalised < 0.0031308) {
+            return 255.0f * (normalised * 12.92);
+        } else {
+            return 255.0f
+                    * (std::pow(normalised * 1.055, 1.0f / 2.4f) - 0.055f);
+        }
     }
 }
 
 
 FSL_MAIN("mix", "AnimRay. Copyright 2010-2020 Kirit Saelensminde")
 (fostlib::ostream &out, fostlib::arguments &args) {
-    animray::film<animray::rgb<uint8_t>> image{
+    animray::film<animray::rgb<uint8_t>> linear{
             width, height, [](auto x, auto y) {
-                auto const left = lerp(top_left, bottom_left, y, height);
-                auto const right = lerp(top_right, bottom_right, y, height);
-                auto const pixel = lerp(left, right, x, width);
+                auto const pixel = pixel_colour(x, y);
                 return animray::rgb<uint8_t>(
-                        quantise(pixel.red()), quantise(pixel.green()),
-                        quantise(pixel.blue()));
+                        linear_clamp(pixel.red()), linear_clamp(pixel.green()),
+                        linear_clamp(pixel.blue()));
             }};
-    animray::targa("mix-linear.tga", image);
+    animray::targa("mix-linear.tga", linear);
+    animray::film<animray::rgb<uint8_t>> non_linear{
+            width, height, [](auto x, auto y) {
+                auto const pixel = pixel_colour(x, y);
+                return animray::rgb<uint8_t>(
+                        non_linear_clamp(pixel.red()),
+                        non_linear_clamp(pixel.green()),
+                        non_linear_clamp(pixel.blue()));
+            }};
+    animray::targa("mix-non_linear.tga", non_linear);
     return 0;
 }
