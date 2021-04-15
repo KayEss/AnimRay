f5fd632979998e7803bcc1c522cba482e838d3a9
Kirit Sælensminde <kirit@kirit.com>
2021-04-15 15:35:06 +0700

    Can now be built from a clone on its own.

---
+++ requirements.cmake
@@ -0,0 +1,23 @@
+include(FetchContent)
+
+FetchContent_Declare(
+        felspar-test
+        GIT_REPOSITORY https://github.com/Felspar/test.git
+        GIT_TAG main
+    )
+FetchContent_GetProperties(felspar-test)
+if(NOT felspar-test_POPULATED)
+    FetchContent_Populate(felspar-test)
+    add_subdirectory(${felspar-test_SOURCE_DIR} ${felspar-test_BINARY_DIR})
+endif()
+
+FetchContent_Declare(
+        felspar-exceptions
+        GIT_REPOSITORY https://github.com/Felspar/exceptions.git
+        GIT_TAG main
+    )
+FetchContent_GetProperties(felspar-exceptions)
+if(NOT felspar-exceptions_POPULATED)
+    FetchContent_Populate(felspar-exceptions)
+    add_subdirectory(${felspar-exceptions_SOURCE_DIR} ${felspar-exceptions_BINARY_DIR})
+endif()
